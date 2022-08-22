cbd0c2ec944a41ec7ef06ce33a41de6127a13775
Ruslan Akhmadiev <ahmadievrr@gmail.com>
2022-08-22 13:19:49 +0300

    third try1

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -34,14 +34,14 @@ const BurgerConstructor = (props) => {
         )    
       } else if(index !== arr.length - 1) {
         return(
-        <div key={index} className={`${styles.ingredient} mt-4`}>
+        <div key={item._id} className={`${styles.ingredient} mt-4`}>
           <DragIcon type="primary"/>
           <ConstructorElement text={item.name} thumbnail={item.image} price={item.price}/>
         </div>
         )
       } else {
         return(
-          <div key={index}>
+          <div key={item._id}>
             <div  className={`${styles.ingredient} mt-4`}>
               <DragIcon type="primary"/>
               <ConstructorElement text={item.name} thumbnail={item.image} price={item.price}/>
